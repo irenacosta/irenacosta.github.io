ca235db94a785923440c3f70a135a1eb0a9a6576
Irena Costa <irena.costa@gmail.com>
2022-01-13 00:27:09 -0300

    Add files via upload
    função onclick para redirecionar após click no card

---
+++ js/personagemonclick.js
@@ -0,0 +1,11 @@
+function redirecionarpeter() {
+    location.href = "../vingadores/peter1.html"
+}
+
+function redirecionarstrange() {
+    location.href = "../vingadores/strange1.html"
+}
+
+function redirecionarwanda() {
+    location.href = "../vingadores/wanda1.html"
+}
